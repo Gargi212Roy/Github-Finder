814aec123bb6142d6aa374e100dd13fddd249297
itsgargiroy <itsgargiroy@gmail.com>
2023-02-18 13:09:13 +0530

    about updated

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,17 +6,24 @@ function About(props) {
     <div>
       <h1 className="text-6xl mb-4">GitHub Finder</h1>
       <p className="mb-4 text-2xl font-light">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam,
-        debitis distinctio? Doloremque autem optio molestias non odit veniam
-        sapiente sequi dolorum asperiores?
+        GitHub-Finder is a website from which you can search any user who has an
+        active Github Account. On searching such a user you'll be displayed all
+        the possible users with similar names including the user you are looking
+        for. On clicking any particular user you'll be shown some current
+        repositories, followers, following and a couple of other details. I hope
+        you guys enjoy it.
       </p>
+
       <p className="text-lg text-gray-400">
         Version <span className="text-white">1.0.0</span>{" "}
       </p>
       <p className="text-lg text-gray-400">
         Layout By:
-        <a href="'https://twitter.com/hassibmoddasser" className="text-white">
-          Hassib Moddasser
+        <a
+          href="https://www.linkedin.com/in/gargi-roy-55a74b205/"
+          className="text-white"
+        >
+          Gargi Roy
         </a>
       </p>
     </div>
